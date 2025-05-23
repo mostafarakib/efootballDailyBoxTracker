45f074b06700aabce149a9783708d78f05ab904e
mostafarakib <mostafa.rakib.10@gmail.com>
2025-05-23 22:57:16 +0600

    configured redux for authentication

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-yellow-600 text-3xl">hello</h1>
+    </>
+  );
+}
+
+export default App;
